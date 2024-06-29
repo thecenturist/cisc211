724016ee395c99a474d8a7940d68be6d5470c309
AJ <a.anjolaoluwa@yahoo.com>
2024-06-29 04:54:21 +0000

    Finished homework

--- w2c.asm
+++ w2c.asm
@@ -1,16 +1,17 @@
 global _start
 
-segment .bss:
+segment .bss
 	result RESB 0 
 
-segment .data:
+segment .data
 	var1 DW 10 ; initialize var1
 	var2 DW 15 ; initialize var2
 
 _start:
 	mov eax, [var1] ; move the bytes in memory at var1 into eax
 	add eax, [var2] ; add the bytes in memory at var2 to eax
-	mov ebx, eax
+	mov [result], eax
+	mov ebx, [result]
 
 	mov eax, 1 ; sys_exit
 	int 0x80 ; program exit
